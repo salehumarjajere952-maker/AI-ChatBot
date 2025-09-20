d7a5f7ec521dbc3840b7867a2d25b50a79698914
salehumarjajere952-maker <salehumarjajere952@gmail.com>
2025-09-20 19:02:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+print("Hello! Welcome to my chatbot")
+answer = input("Are you excited to interact with this program? ")
+if (answer == "yes"):
+  print ("I'm so excited too!")
+elif (answer == "no"):
+  print ("Aww, well I hope I can change your mind!")
+else:
+  print ("Hmm.. I seem to not comprehend your answer.")
